6280a92d4065e4fc2738904b7e9652208c7333a7
Saiprasad Pagar <saiprasadpagar@gmail.com>
2023-05-06 14:29:00 +0530

    Delete fun added"

--- src/Components/Home/Cards/card4.jsx
+++ src/Components/Home/Cards/card4.jsx
@@ -1,8 +1,12 @@
-import { React, useState } from "react";
+import { React, useEffect, useState } from "react";
 
 const Card4 = () => {
   const [currentValue, setCurrentValue] = useState("");
   const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
 
   const onChangeHandler = (event) => {
     const { value } = event.target;
@@ -13,19 +17,64 @@ const Card4 = () => {
     setData([...data, currentValue]);
   };
 
+
+
+//method 2
+  const handleDelete = (index) => { 
+    const cloneData = JSON.parse(JSON.stringify(data)); //Deep Copy
+   let newData = cloneData.splice(index , 1 )   
+
+console.log(data)
+console.log(newData)
+   
+setData(cloneData)
+return data
+  }
+    
+    
+    //method:1
+  //   const newTodos = data.filter((elem, i) => {
+  //     return i !== index;
+  //   });
+
+    // setData(newTodos);
+  //   // return data;
+  // };
+
   return (
     <>
       <h3>To Do App</h3>
+      <p>All Cards Keep Save</p>
+      <p>
+        Delete Functionalitiy & When click on card get popup with pop delete
+        cross button
+      </p>
       <input typr="text" onChange={onChangeHandler} />
-      <button onClick={addHandler}>Add List</button><br/>
-      <div className="todo-cards">
-      {data.map((elem) => {
-        return (
-          
-            <div className="todo-li">{elem}</div>
-          
-        );
-      })}
+      <button onClick={addHandler}>Add List</button>
+      <br />
+
+      <div className="todo-grantparent">
+        <div className="todo-parent">
+          {data.map((elem, index) => {
+            return (
+              <>
+                <div id={index} className="todo-child">
+                  {`${elem} index = ${index}`}
+                  <button className="todo-cross-button">X</button>
+
+                  <button
+                    onClick={() => {
+                      handleDelete(index);
+                    }}
+                    className="todo-delete-button"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </>
+            );
+          })}
+        </div>
       </div>
     </>
   );
